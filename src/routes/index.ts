e216547fe248cbbdbb75b4edf6edc3683d476825
Abdelrahman Ashraf <mr.abdo1213@gmail.com>
2024-06-20 11:41:02 +0400

    logout commented

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Express } from 'express';
 import usersRoutes from './users.route';
-import authRoute from './auth.route';
+// import authRoute from './auth.route';
 import boxRoute from './box.route';
 import vendorRoute from './vendor.route';
 import deliveryRoute from './delivery.route';
@@ -8,7 +8,7 @@ import operationsRoute from './operations.route';
 
 const mountRoutes = (app: Express) => {
   app.use('/api/users', usersRoutes);
-  app.use('/api/auth', authRoute);
+  // app.use('/api/auth', authRoute);
   app.use('/api/box', boxRoute);
   app.use('/api/vendor', vendorRoute);
   app.use('/api/delivery', deliveryRoute);

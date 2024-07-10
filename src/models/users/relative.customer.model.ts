18f0dbfe85ab27edf0f13678bb2d89f7fb8873ab
Abdelrahman <abdelrahmanaosman99@gmail.com>
2024-07-10 12:12:49 +0400

    fix

--- src/models/users/relative.customer.model.ts
+++ src/models/users/relative.customer.model.ts
@@ -44,12 +44,13 @@ class RelativeCustomerModel {
   }
 
   // get all relative customers
-  async getMany(): Promise<RelativeCustomer[]> {
+  async getMany(user: string): Promise<RelativeCustomer[]> {
     const connection = await db.connect();
 
     try {
-      const sql = 'SELECT * FROM Relative_Customer';
-      const result = await connection.query(sql);
+      const sql =
+        'SELECT users.user_name, users.phone_number, Relative_Customer.is_active, Relative_Customer.relation FROM Relative_Customer INNER JOIN users ON users.id=Relative_Customer.relative_customer_id WHERE Relative_Customer.customer_id=$1';
+      const result = await connection.query(sql, [user]);
       return result.rows as RelativeCustomer[];
     } catch (error) {
       throw new Error((error as Error).message);
@@ -176,6 +177,8 @@ class RelativeCustomerModel {
       connection.release();
     }
   }
+
+  //
 }
 
 export default RelativeCustomerModel;

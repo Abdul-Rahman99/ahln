3beeb8deff80abd58fafaec0c33e96fcc473b54d
Abdelrahman Ashraf <abdelrahmanAosman99@gmail.com>
2024-07-13 13:17:11 +0400

    Fix update user responsea

--- src/controllers/user/pin.controller.ts
+++ src/controllers/user/pin.controller.ts
@@ -1,93 +1,93 @@
-import { Request, Response, NextFunction } from 'express';
-import asyncHandler from '../../middlewares/asyncHandler';
-import { PIN } from '../../types/pin.type';
-import i18n from '../../config/i18n';
-import ResponseHandler from '../../utils/responsesHandler';
-import PINModel from '../../models/users/pin.model';
-import authHandler from '../../utils/authHandler';
-import BoxModel from '../../models/box/box.model';
+// import { Request, Response, NextFunction } from 'express';
+// import asyncHandler from '../../middlewares/asyncHandler';
+// import { PIN } from '../../types/pin.type';
+// import i18n from '../../config/i18n';
+// import ResponseHandler from '../../utils/responsesHandler';
+// import PINModel from '../../models/users/pin.model';
+// import authHandler from '../../utils/authHandler';
+// import BoxModel from '../../models/box/box.model';
 
-const boxModel = new BoxModel();
-const pinModel = new PINModel();
+// const boxModel = new BoxModel();
+// const pinModel = new PINModel();
 
-export const createPin = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const newPin: PIN = req.body;
-      const user = await authHandler(req, res, next);
-      const boxExist = await boxModel.getOne(newPin.box_id);
-      if (!boxExist) {
-        return ResponseHandler.badRequest(res, i18n.__('BOX_ID_INVALID'));
-      }
-      const createdPin = await pinModel.createPIN(newPin, user);
-      ResponseHandler.success(
-        res,
-        i18n.__('PIN_CREATED_SUCCESSFULLY'),
-        createdPin,
-      );
-    } catch (error) {
-      next(error);
-      ResponseHandler.badRequest(res, (error as Error).message);
-    }
-  },
-);
+// export const createPin = asyncHandler(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const newPin: PIN = req.body;
+//       const user = await authHandler(req, res, next);
+//       const boxExist = await boxModel.getOne(newPin.box_id);
+//       if (!boxExist) {
+//         return ResponseHandler.badRequest(res, i18n.__('BOX_ID_INVALID'));
+//       }
+//       const createdPin = await pinModel.createPIN(newPin, user);
+//       ResponseHandler.success(
+//         res,
+//         i18n.__('PIN_CREATED_SUCCESSFULLY'),
+//         createdPin,
+//       );
+//     } catch (error) {
+//       next(error);
+//       ResponseHandler.badRequest(res, (error as Error).message);
+//     }
+//   },
+// );
 
-export const getAllPinByUser = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const user = await authHandler(req, res, next);
-      const pins = await pinModel.getAllPinByUser(user);
-      ResponseHandler.success(
-        res,
-        i18n.__('PINS_RETRIEVED_SUCCESSFULLY'),
-        pins,
-      );
-    } catch (error) {
-      next(error);
-      ResponseHandler.badRequest(res, (error as Error).message);
-    }
-  },
-);
+// export const getAllPinByUser = asyncHandler(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const user = await authHandler(req, res, next);
+//       const pins = await pinModel.getAllPinByUser(user);
+//       ResponseHandler.success(
+//         res,
+//         i18n.__('PINS_RETRIEVED_SUCCESSFULLY'),
+//         pins,
+//       );
+//     } catch (error) {
+//       next(error);
+//       ResponseHandler.badRequest(res, (error as Error).message);
+//     }
+//   },
+// );
 
-export const getOnePinByUser = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const pinId = parseInt(req.params.id, 10);
-      const user = await authHandler(req, res, next);
-      const pin = await pinModel.getOnePinByUser(pinId, user);
-      ResponseHandler.success(res, i18n.__('PIN_RETRIEVED_SUCCESSFULLY'), pin);
-    } catch (error) {
-      next(error);
-      ResponseHandler.badRequest(res, (error as Error).message);
-    }
-  },
-);
+// export const getOnePinByUser = asyncHandler(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const pinId = parseInt(req.params.id, 10);
+//       const user = await authHandler(req, res, next);
+//       const pin = await pinModel.getOnePinByUser(pinId, user);
+//       ResponseHandler.success(res, i18n.__('PIN_RETRIEVED_SUCCESSFULLY'), pin);
+//     } catch (error) {
+//       next(error);
+//       ResponseHandler.badRequest(res, (error as Error).message);
+//     }
+//   },
+// );
 
-export const deleteOnePinByUser = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const pinId = parseInt(req.params.id, 10);
-      const user = await authHandler(req, res, next);
-      const pin = await pinModel.deleteOnePinByUser(pinId, user);
-      ResponseHandler.success(res, i18n.__('PIN_DELETED_SUCCESSFULLY'), pin);
-    } catch (error) {
-      next(error);
-      ResponseHandler.badRequest(res, (error as Error).message);
-    }
-  },
-);
+// export const deleteOnePinByUser = asyncHandler(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const pinId = parseInt(req.params.id, 10);
+//       const user = await authHandler(req, res, next);
+//       const pin = await pinModel.deleteOnePinByUser(pinId, user);
+//       ResponseHandler.success(res, i18n.__('PIN_DELETED_SUCCESSFULLY'), pin);
+//     } catch (error) {
+//       next(error);
+//       ResponseHandler.badRequest(res, (error as Error).message);
+//     }
+//   },
+// );
 
-export const updateOnePinByUser = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const pinId = parseInt(req.params.id, 10);
-      const user = await authHandler(req, res, next);
-      const pinData: Partial<PIN> = req.body;
-      const pin = await pinModel.updatePinByUser(pinData, pinId, user);
-      ResponseHandler.success(res, i18n.__('PIN_UPDATED_SUCCESSFULLY'), pin);
-    } catch (error) {
-      next(error);
-      ResponseHandler.badRequest(res, (error as Error).message);
-    }
-  },
-);
+// export const updateOnePinByUser = asyncHandler(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const pinId = parseInt(req.params.id, 10);
+//       const user = await authHandler(req, res, next);
+//       const pinData: Partial<PIN> = req.body;
+//       const pin = await pinModel.updatePinByUser(pinData, pinId, user);
+//       ResponseHandler.success(res, i18n.__('PIN_UPDATED_SUCCESSFULLY'), pin);
+//     } catch (error) {
+//       next(error);
+//       ResponseHandler.badRequest(res, (error as Error).message);
+//     }
+//   },
+// );
--- src/models/users/pin.model.ts
+++ src/models/users/pin.model.ts
@@ -1,147 +1,173 @@
-import { PIN } from '../../types/pin.type';
-import db from '../../config/database';
+// import { PIN } from '../../types/pin.type';
+// import db from '../../config/database';
 
-class PINModel {
-  // create PIN
-  async createPIN(pinData: Partial<PIN>, user: string): Promise<PIN> {
-    const connection = await db.connect();
-    try {
-      const createdAt = new Date();
-      const updatedAt = new Date();
+// class PINModel {
+//   // create PIN
+//   async createPIN(pinData: Partial<PIN>, user: string): Promise<PIN> {
+//     const connection = await db.connect();
+//     try {
+//       const createdAt = new Date();
+//       const updatedAt = new Date();
 
-      const sqlFields = [
-        'createdAt',
-        'updatedAt',
-        'reciepent_email',
-        'title',
-        'is_active',
-        'time_range',
-        'day_range',
-        'box_id',
-        'user_id',
-        'type',
-        'passcode',
-      ];
-      const sqlParams = [
-        createdAt,
-        updatedAt,
-        pinData.reciepent_email,
-        pinData.title,
-        pinData.is_active || true,
-        pinData.time_range,
-        pinData.day_range,
-        pinData.box_id,
-        user,
-        pinData.type,
-        pinData.passcode,
-      ];
-      const sql = `INSERT INTO PIN (${sqlFields.join(', ')}) 
-                  VALUES (${sqlParams.map((_, index) => `$${index + 1}`).join(', ')}) 
-                  RETURNING *`;
+//       const sqlFields = [
+//         'createdAt',
+//         'updatedAt',
+//         'reciepent_email',
+//         'title',
+//         'is_active',
+//         'time_range',
+//         'day_range',
+//         'box_id',
+//         'user_id',
+//         'type',
+//         'passcode',
+//       ];
+//       const sqlParams = [
+//         createdAt,
+//         updatedAt,
+//         pinData.reciepent_email,
+//         pinData.title,
+//         pinData.is_active || true,
+//         pinData.time_range,
+//         pinData.day_range,
+//         pinData.box_id,
+//         user,
+//         pinData.type,
+//         pinData.passcode,
+//       ];
+//       const sql = `INSERT INTO PIN (${sqlFields.join(', ')}) 
+//                   VALUES (${sqlParams.map((_, index) => `$${index + 1}`).join(', ')}) 
+//                   RETURNING *`;
 
-      const result = await connection.query(sql, sqlParams);
+//       const result = await connection.query(sql, sqlParams);
 
-      return result.rows[0];
-    } catch (error) {
-      throw new Error((error as Error).message);
-    } finally {
-      connection.release();
-    }
-  }
+//       return result.rows[0];
+//     } catch (error) {
+//       throw new Error((error as Error).message);
+//     } finally {
+//       connection.release();
+//     }
+//   }
 
-  // get all pin management
-  async getAllPinByUser(user: string): Promise<PIN[]> {
-    const connection = await db.connect();
-    try {
-      const sql = 'SELECT * FROM PIN WHERE user_id=$1';
-      const result = await connection.query(sql, [user]);
+//   // get all pin management
+//   async getAllPinByUser(user: string): Promise<PIN[]> {
+//     const connection = await db.connect();
+//     try {
+//       const sql = 'SELECT * FROM PIN WHERE user_id=$1';
+//       const result = await connection.query(sql, [user]);
 
-      return result.rows as PIN[];
-    } catch (error) {
-      throw new Error((error as Error).message);
-    } finally {
-      connection.release();
-    }
-  }
+//       return result.rows as PIN[];
+//     } catch (error) {
+//       throw new Error((error as Error).message);
+//     } finally {
+//       connection.release();
+//     }
+//   }
 
-  // get one pin management
-  async getOnePinByUser(id: number, user: string): Promise<PIN[]> {
-    const connection = await db.connect();
-    try {
-      const sql = 'SELECT * FROM PIN WHERE id=$1 AND user_id=$2';
-      const result = await connection.query(sql, [id, user]);
-      return result.rows as PIN[];
-    } catch (error) {
-      throw new Error((error as Error).message);
-    } finally {
-      connection.release();
-    }
-  }
+//   // get one pin management
+//   async getOnePinByUser(id: number, user: string): Promise<PIN[]> {
+//     const connection = await db.connect();
+//     try {
+//       const sql = 'SELECT * FROM PIN WHERE id=$1 AND user_id=$2';
+//       const result = await connection.query(sql, [id, user]);
+//       return result.rows as PIN[];
+//     } catch (error) {
+//       throw new Error((error as Error).message);
+//     } finally {
+//       connection.release();
+//     }
+//   }
 
-  // delete Pin
-  async deleteOnePinByUser(id: number, user: string): Promise<PIN[]> {
-    const connection = await db.connect();
-    try {
-      const sql = 'DELETE FROM PIN WHERE id=$1 AND user_id=$2';
-      const result = await connection.query(sql, [id, user]);
-      return result.rows as PIN[];
-    } catch (error) {
-      throw new Error((error as Error).message);
-    } finally {
-      connection.release();
-    }
-  }
+//   // delete Pin
+//   async deleteOnePinByUser(id: number, user: string): Promise<PIN[]> {
+//     const connection = await db.connect();
+//     try {
+//       const sql = 'DELETE FROM PIN WHERE id=$1 AND user_id=$2';
+//       const result = await connection.query(sql, [id, user]);
+//       return result.rows as PIN[];
+//     } catch (error) {
+//       throw new Error((error as Error).message);
+//     } finally {
+//       connection.release();
+//     }
+//   }
 
-  // update Pin
-  async updatePinByUser(
-    pinData: Partial<PIN>,
-    id: number,
-    user: string,
-  ): Promise<PIN> {
-    const connection = await db.connect();
-    try {
-      // Check if the pin exists for this user
-      const checkSql = 'SELECT * FROM PIN WHERE id=$1 AND user_id=$2';
-      const checkResult = await connection.query(checkSql, [id, user]);
+//   // update Pin
+//   async updatePinByUser(
+//     pinData: Partial<PIN>,
+//     id: number,
+//     user: string,
+//   ): Promise<PIN> {
+//     const connection = await db.connect();
+//     try {
+//       // Check if the pin exists for this user
+//       const checkSql = 'SELECT * FROM PIN WHERE id=$1 AND user_id=$2';
+//       const checkResult = await connection.query(checkSql, [id, user]);
 
-      if (checkResult.rows.length === 0) {
-        throw new Error(`PIN with ID ${id} does not exist`);
-      }
-      const queryParams: unknown[] = [];
-      let paramIndex = 1;
+//       if (checkResult.rows.length === 0) {
+//         throw new Error(`PIN with ID ${id} does not exist`);
+//       }
+//       const queryParams: unknown[] = [];
+//       let paramIndex = 1;
 
-      const updatedAt = new Date();
+//       const updatedAt = new Date();
 
-      const updateFields = Object.keys(pinData)
-        .map((key) => {
-          if (
-            pinData[key as keyof PIN] !== undefined &&
-            key !== 'id' &&
-            key !== 'createdAt'
-          ) {
-            queryParams.push(pinData[key as keyof PIN]);
-            return `${key}=$${paramIndex++}`;
-          }
-          return null;
-        })
-        .filter((field) => field !== null);
+//       const updateFields = Object.keys(pinData)
+//         .map((key) => {
+//           if (
+//             pinData[key as keyof PIN] !== undefined &&
+//             key !== 'id' &&
+//             key !== 'createdAt'
+//           ) {
+//             queryParams.push(pinData[key as keyof PIN]);
+//             return `${key}=$${paramIndex++}`;
+//           }
+//           return null;
+//         })
+//         .filter((field) => field !== null);
 
-      queryParams.push(updatedAt); // Add the updatedAt timestamp
-      updateFields.push(`updatedAt=$${paramIndex++}`); // Include updatedAt field in the update query
+//       queryParams.push(updatedAt); // Add the updatedAt timestamp
+//       updateFields.push(`updatedAt=$${paramIndex++}`); // Include updatedAt field in the update query
 
-      queryParams.push(id); // Add the pin ID to the query parameters
+//       queryParams.push(id); // Add the pin ID to the query parameters
 
-      const sql = `UPDATE PIN SET ${updateFields.join(', ')} WHERE id=$${paramIndex} RETURNING *`;
+//       const sql = `UPDATE PIN SET ${updateFields.join(', ')} WHERE id=$${paramIndex} RETURNING *`;
 
-      const result = await connection.query(sql, queryParams);
+//       const result = await connection.query(sql, queryParams);
 
-      return result.rows[0] as PIN;
-    } catch (error) {
-      throw new Error((error as Error).message);
-    } finally {
-      connection.release();
-    }
-  }
-}
+//       return result.rows[0] as PIN;
+//     } catch (error) {
+//       throw new Error((error as Error).message);
+//     } finally {
+//       connection.release();
+//     }
+//   }
 
-export default PINModel;
+//   async checkPIN(passcode: string, box_id: string): Promise<boolean> {
+//     const connection = await db.connect();
+//     try {
+//       if (!passcode) {
+//         throw new Error('Please provide a PIN');
+//       }
+
+//       // Check if PIN exists and is active and passcode is correct
+//       const pinResult = await connection.query(
+//         'SELECT time_range, day_range FROM PIN WHERE passcode = $1 AND is_active = TRUE AND  box_id = $2',
+//         [passcode, box_id],
+//       );
+
+//       if (pinResult.rows.length === 0) {
+//         throw new Error('PIN not found or pin is not activated');
+//       }
+
+//       const { time_range, day_range } = pinResult.rows[0];
+//       const timeNow = new Date()
+//     } catch (error) {
+//       throw new Error((error as Error).message);
+//     } finally {
+//       connection.release();
+//     }
+//   }
+// }
+
+// export default PINModel;

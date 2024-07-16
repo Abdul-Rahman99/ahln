e4c8374e1622987852d1a6ede9a6af7c283f8ea1
Abdelrahman Ashraf <abdelrahmanAosman99@gmail.com>
2024-07-16 16:42:04 +0400

    Fix

--- src/models/users/pin.model.ts
+++ src/models/users/pin.model.ts
@@ -63,7 +63,7 @@ class PINModel {
     const connection = await db.connect();
     try {
       const sql =
-        'SELECT b.box_label, PIN.reciepent_email, PIN.title, PIN.is_active, PIN.time_range, PIN.day_range, PIN.box_id, PIN.user_id, PIN.type FROM PIN INNER JOIN Box as b ON b.id=PIN.box_id WHERE user_id=$1';
+        'SELECT b.box_label, PIN.* FROM PIN INNER JOIN Box as b ON b.id=PIN.box_id WHERE user_id=$1';
       const result = await connection.query(sql, [user]);
 
       return result.rows as PIN[];

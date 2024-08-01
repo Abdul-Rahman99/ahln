17ca600ab6f9a1a903ee37aa962299a3a14395c7
ali <straoid@gmail.com>
2024-08-01 10:56:03 +0300

    feat: add apif for livestreaming post and get

--- src/controllers/delivery/image.controller.ts
+++ src/controllers/delivery/image.controller.ts
@@ -4,10 +4,40 @@ import asyncHandler from '../../middlewares/asyncHandler';
 import ResponseHandler from '../../utils/responsesHandler';
 import i18n from '../../config/i18n';
 import { uploadSingleImage } from '../../middlewares/uploadSingleImage';
+import { handleLiveStream } from '../../middlewares/handleLiveStreamImage';
+
 import SystemLogModel from '../../models/logs/system.log.model';
 import authHandler from '../../utils/authHandler';
 
 const systemLog = new SystemLogModel();
+
+export const liveStream = asyncHandler(async (req: Request, res: Response) => {
+  const user = await authHandler(req, res);
+
+  try {
+    handleLiveStream('image')(req, res, async (err: any) => {
+      if (err) {
+        const source = 'liveStream';
+        systemLog.createSystemLog(user, (err as Error).message, source);
+        return ResponseHandler.badRequest(res, err.message);
+      }
+      if (!req.file) {
+        const source = 'liveStream';
+        systemLog.createSystemLog(user, (err as Error).message, source);
+        return ResponseHandler.badRequest(res, i18n.__('NO_FILE_PROVIDED'));
+      }
+
+      ResponseHandler.success(res, i18n.__('IMAGE_UPLOADED_SUCCESSFULLY'), {
+        file: req.file,
+      });
+    });
+  } catch (error: any) {
+    const source = 'liveStream';
+    systemLog.createSystemLog(user, (error as Error).message, source);
+    ResponseHandler.badRequest(res, error.message);
+    // next(error);
+  }
+});
 
 export const uploadImage = asyncHandler(async (req: Request, res: Response) => {
   const user = await authHandler(req, res);
@@ -36,3 +66,23 @@ export const uploadImage = asyncHandler(async (req: Request, res: Response) => {
     // next(error);
   }
 });
+
+export const getLiveStreamImage = asyncHandler(
+  async (req: Request, res: Response) => {
+    const user = await authHandler(req, res);
+
+    try {
+      const image = process.env.BASE_URL + "/uploads/liveImage-"+req.params.id+'.png';
+      ResponseHandler.success(
+        res,
+        i18n.__('LIVE_IMAGE_RETRIVED_SUCCESSFULLY'),
+        image,
+      );
+    } catch (error: any) {
+      const source = 'getDPFavListByUser';
+      systemLog.createSystemLog(user, (error as Error).message, source);
+      ResponseHandler.badRequest(res, error.message);
+      // next(error);
+    }
+  },
+);
--- src/routes/delivery/image.route.ts
+++ src/routes/delivery/image.route.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { uploadImage } from '../../controllers/delivery/image.controller';
+import { liveStream } from '../../controllers/delivery/image.controller';
+import { getLiveStreamImage } from '../../controllers/delivery/image.controller';
+
 
 const router = Router();
 
 router.post('/upload', uploadImage);
+router.post('/livestream', liveStream);
+router.get('/getLiveStreamImage/:id', getLiveStreamImage);
+
 
 export default router;

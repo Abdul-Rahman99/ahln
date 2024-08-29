a964c984bc8a9e2c15628c0e26fb9d5b733811ff
Abdelrahman Ashraf <abdelrahmanAosman99@gmail.com>
2024-08-29 16:28:32 +0400

    Add the address lat lang

--- src/models/delivery/delivery.package.model.ts
+++ src/models/delivery/delivery.package.model.ts
@@ -98,7 +98,8 @@ class DeliveryPackageModel {
 
       const sql = `INSERT INTO Delivery_Package (${sqlFields.join(', ')}) 
                 VALUES (${sqlParams.map((_, index) => `$${index + 1}`).join(', ')}) 
-                RETURNING id, tracking_number, box_id, box_locker_id, shipping_company_id, shipment_status, title AS name, delivery_pin, description, other_shipping_company, otp`;
+                RETURNING id, tracking_number, box_id, box_locker_id, shipping_company_id, shipment_status, title AS name, delivery_pin, description, other_shipping_company, otp,
+                createdAt, updatedAt, customer_id, vendor_id, delivery_id, is_delivered, box_locker_string, address_id`;
 
       const result = await connection.query(sql, sqlParams);
 

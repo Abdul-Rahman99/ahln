43bc821111fcc7bc6b30405728f4b2290ae5ab66
Abdelrahman Ashraf <abdelrahmanAosman99@gmail.com>
2024-09-30 09:42:40 +0400

    check box exists offline otps

--- src/controllers/delivery/offline.otps.controller.ts
+++ src/controllers/delivery/offline.otps.controller.ts
@@ -6,7 +6,9 @@ import ResponseHandler from '../../utils/responsesHandler';
 import OfflineOtps from '../../models/delivery/offline.otps.model';
 import SystemLogModel from '../../models/logs/system.log.model';
 import i18n from '../../config/i18n';
+import BoxModel from '../../models/box/box.model';
 
+const boxModel = new BoxModel();
 const offlineOTPsModel = new OfflineOtps();
 const systemLog = new SystemLogModel();
 
@@ -19,6 +21,16 @@ export const createOfflineOTPs = asyncHandler(
 
     try {
       const boxId = req.body.box_id;
+
+      //check if the box exists
+      const boxExist = await boxModel.getOne(boxId);
+
+      if (!boxExist) {
+        const source = 'createOfflineOTPs';
+        systemLog.createSystemLog(user, 'Box Does Not Exist', source);
+        return ResponseHandler.badRequest(res, i18n.__('BOX_DOES_NOT_EXIST'));
+      }
+
       const otp = await offlineOTPsModel.createOfflineOtps(boxId);
       ResponseHandler.success(res, i18n.__('OTP_CRAETED_SUCCESSFULLY'), otp);
     } catch (error: any) {
@@ -39,6 +51,14 @@ export const getAllOfflineOTPs = asyncHandler(
 
     try {
       const boxId = req.params.box_id;
+      //check if the box exists
+      const boxExist = await boxModel.getOne(boxId);
+
+      if (!boxExist) {
+        const source = 'createOfflineOTPs';
+        systemLog.createSystemLog(user, 'Box Does Not Exist', source);
+        return ResponseHandler.badRequest(res, i18n.__('BOX_DOES_NOT_EXIST'));
+      }
       const otps = await offlineOTPsModel.getAllOfflineOtps(boxId);
       ResponseHandler.success(
         res,
